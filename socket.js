dc9754c21dbdbae5cedefd0bf9cca11816bfbe3c
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-07 22:17:34 +0100

    Update and rename socket to socket.js

---
+++ socket.js
@@ -0,0 +1,24 @@
+io = require('socket.io');
+// Create SocketIO instance, connect
+var socket = new io.Socket('192.168.1.103',{
+	port: 3001
+});
+socket.connect(); 
+
+// Add a connect listener
+socket.on('connect',function() {
+	console.log('Client has connected to the server!');
+});
+// Add a connect listener
+socket.on('message',function(data) {
+	console.log('Received a message from the server!',data);
+});
+// Add a disconnect listener
+socket.on('disconnect',function() {
+	console.log('The client has disconnected!');
+});
+
+// Sends a message to the server via sockets
+function sendMessageToServer(message) {
+	socket.send(message);
+}
