65ba82f5adf82639f352cd9b0687af60892caf1c
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-07 22:54:28 +0100

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,7 +3,7 @@ var io = require('socket.io-client');
 var serverUrl = 'http://192.168.1.103:3001';
 var conn = io.connect(serverUrl);
  
-var p1 = new Date().getTime();
+var p1 = 'hoi';
 conn.emit('call', p1, function(resp, data) {
     console.log('server sent resp code ' + resp);
 });
