a69926f7efbd31c29275c6cca8cef3943e9dbcf4
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-07 22:47:41 +0100

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,23 +1,9 @@
 // Create SocketIO instance, connect
-var socket = new io.Socket('192.168.1.103',{
-	port: 3001
+var io = require('socket.io-client');
+var serverUrl = 'http://192.168.1.103:3001';
+var conn = io.connect(serverUrl);
+ 
+var p1 = 'hello';
+conn.emit('call', p1, function(resp, data) {
+    console.log('server sent resp code ' + resp);
 });
-socket.connect(); 
-
-// Add a connect listener
-socket.on('connect',function() {
-	console.log('Client has connected to the server!');
-});
-// Add a connect listener
-socket.on('message',function(data) {
-	console.log('Received a message from the server!',data);
-});
-// Add a disconnect listener
-socket.on('disconnect',function() {
-	console.log('The client has disconnected!');
-});
-
-// Sends a message to the server via sockets
-function sendMessageToServer(message) {
-	socket.send(message);
-}
