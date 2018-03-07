a78739ad47e78d96faf94b6ecc544458c525b306
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-07 23:07:32 +0100

    Update socket.js

--- socket.js
+++ socket.js
@@ -3,8 +3,10 @@ var io = require('socket.io-client');
 var serverUrl = 'http://192.168.1.103:3001';
 var conn = io.connect(serverUrl);
  console.log('start');
-
-var p1 = 'hoi';
+var interval = setInterval(function() {
+	var p1 = 'This is a message from the pi!  ' + new Date().getTime();
 conn.emit('message', p1, function(resp, data) {
     console.log('server sent resp code ' + resp);
 });
+},5000);
+
