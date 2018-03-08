8f4d98763ae69fd7fe8e19ce081ef13f8465a919
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-08 20:24:50 +0100

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,12 +1,10 @@
-// Create SocketIO instance, connect
-var io = require('socket.io-client');
-var serverUrl = 'http://192.168.1.103:3001';
-var conn = io.connect(serverUrl);
+const ws = new WebSocket('ws://192.168.1.103/Inventory');
  console.log('start');
-var interval = setInterval(function() {
+ws.on('open', function open() {
+	var interval = setInterval(function() {
 	var p1 = 'This is a message from the pi!  ' + new Date().getTime();
-conn.emit('message', p1, function(resp, data) {
-    console.log('server sent resp code ' + resp);
-});
+  ws.send(p1);
 },5000);
+});
+
 
