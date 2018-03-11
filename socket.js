3ad1bf1daa02afe0aae186db89fb6cf22dcd87c7
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-11 17:10:26 +0100

    Update socket.js

--- socket.js
+++ socket.js
@@ -2,14 +2,90 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://192.168.0.111:3001/Inventory');
  console.log('start');
 ws.on('open', function open() {
-	var interval = setInterval(function() {
-	var p1 = Math.floor(Math.random() * Math.floor(10))
-  ws.send(p1);
-},1000);
+	console.log('open');
+ 
 });
 
 ws.on('message', function incoming(data) {
   console.log(data);
 });
 
+var bleno = require('bleno');
+ 
+// Once bleno starts, begin advertising our BLE address
+bleno.on('stateChange', function(state) {
+    console.log('State change: ' + state);
+    if (state === 'poweredOn') {
+        bleno.startAdvertising('MyDevice',['12ab']);
+    } else {
+        bleno.stopAdvertising();
+    }
+});
+ 
+// Notify the console that we've accepted a connection
+bleno.on('accept', function(clientAddress) {
+    console.log("Accepted connection from address: " + clientAddress);
+});
+ 
+// Notify the console that we have disconnected from a client
+bleno.on('disconnect', function(clientAddress) {
+    console.log("Disconnected from address: " + clientAddress);
+});
+ 
+// When we begin advertising, create a new service and characteristic
+bleno.on('advertisingStart', function(error) {
+    if (error) {
+        console.log("Advertising start error:" + error);
+    } else {
+        console.log("Advertising start success");
+        bleno.setServices([
+            
+            // Define a new service
+            new bleno.PrimaryService({
+                uuid : '12ab',
+                characteristics : [
+                    
+                    // Define a new characteristic within that service
+                    new bleno.Characteristic({
+                        value : null,
+                        uuid : '34cd',
+                        properties : ['notify', 'read', 'write'],
+                        
+                        // If the client subscribes, we send out a message every 1 second
+                        onSubscribe : function(maxValueSize, updateValueCallback) {
+                            console.log("Device subscribed");
+                            this.intervalId = setInterval(function() {
+                                console.log("Sending: Hi!");
+                                updateValueCallback(new Buffer("Hi!"));
+                            }, 1000);
+                        },
+                        
+                        // If the client unsubscribes, we stop broadcasting the message
+                        onUnsubscribe : function() {
+                            console.log("Device unsubscribed");
+                            clearInterval(this.intervalId);
+                        },
+                        
+                        // Send a message back to the client with the characteristic's value
+                        onReadRequest : function(offset, callback) {
+                            console.log("Read request received");
+                            callback(this.RESULT_SUCCESS, new Buffer("Echo: " + 
+                                    (this.value ? this.value.toString("utf-8") : "")));
+                        },
+                        
+                        // Accept a new value for the characterstic's value
+                        onWriteRequest : function(data, offset, withoutResponse, callback) {
+                            this.value = data;
+                            console.log('Write request: value = ' + this.value.toString("utf-8"));
+				 ws.send(data);
+                            callback(this.RESULT_SUCCESS);
+                        }
+ 
+                    })
+                    
+                ]
+            })
+        ]);
+    }
+});
 
