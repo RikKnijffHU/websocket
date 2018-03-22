f440a6e33ed93d2b00bb126c1a3cfde690eeb086
RikKnijffHU <rik.knijff@student.hu.nl>
2018-03-22 14:22:50 +0100

    added logs

--- topic.js
+++ topic.js
@@ -1,6 +1,8 @@
 var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://172.16.3.169', function(err, conn) {
+  console.log(conn);
+  console.log(err);
   conn.createChannel(function(err, ch) {
     var ex = 'InventoryBus';
     var args = process.argv.slice(2);
@@ -11,6 +13,6 @@ amqp.connect('amqp://172.16.3.169', function(err, conn) {
     ch.publish(ex, key, new Buffer(msg));
     console.log(" [x] Sent %s:'%s'", key, msg);
   });
-
+  console.log("na");
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
